641ed66a48bb3b7b6ea9613ed3606ea854a776da
maxmaxph <maxmaxph@gmail.com>
2023-09-15 15:30:08 +0200

    7 creation pages connect not found && services authinterceptor et user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { CardComponent } from './components/card/card.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { PageConnectComponent } from './pages/page-connect/page-connect.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    CardComponent,
+    PageNotFoundComponent,
+    PageConnectComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/page-connect/page-connect.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-connect',
+  templateUrl: './page-connect.component.html',
+  styleUrls: ['./page-connect.component.css']
+})
+export class PageConnectComponent {
+
+}
