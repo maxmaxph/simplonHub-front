794fd11ad9fc8e4103a7ee10015ad504574c0904
maxmaxph <maxmaxph@gmail.com>
2023-09-22 10:07:20 +0200

    20.2 creation dossiers models et interfaces comments store user

---
+++ src/app/models/comments.ts
@@ -0,0 +1,6 @@
+export interface Comment {
+    id: number;
+    user_id: number;
+    store_id: number;
+    note: number | null;
+}
---
+++ src/app/models/store.ts
@@ -0,0 +1,17 @@
+export interface Store {
+    id: number;
+    name: string;
+    phone: string;
+    categories: any;
+    comments: any;
+    number: string | null;
+    street: string | null;
+    city: string | null;
+    zip: string | null;
+    web: string | null;
+    map: string | null;
+    description: string | null;
+    user_id: number;
+    picture_id: number | null;
+  }
+  
